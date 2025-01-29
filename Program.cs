979a37f8491303ae97db85e70150d40f175d36ee
Asif-36 <130299330+Asif-36@users.noreply.github.com>
2025-01-30 01:15:08 +0600

    300125

--- Program.cs
+++ Program.cs
@@ -11,27 +11,88 @@ namespace HelloWorld
 {
     public class Program
     {
-        public static void Main(string[] args) { 
+       
+             public static void Main()
+        {
+            /* Console.WriteLine("Asif");
+             Console.WriteLine("CSharp");
+             Console.WriteLine("23");
+             Console.WriteLine("Reading Islamic Books");*/
+
+            //Console.WriteLine("Sayad Md Asif\nBangla\n23\nReading Islamic Books");
+
+            string name;
+            int age;
+
+            Console.WriteLine("enter your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Hello! My Name is {name} , I am {age} years old");
 
 
+        
+
+
+
+        /* Console.WriteLine("Enter number 1 to 10");
+         string input = Console.ReadLine() ?? "";
+         //input = input.Trim().ToLower();
+
+         int result = int.Parse(input);
+         while (true)
+         {
+             if (input == "quit")
+             {
+                 Console.WriteLine("GoodBuy");
+                 break;
+             }
+
+             if (!int.TryParse(input, out int result))
+             {
+                 Console.WriteLine("Your input is invalid, please try valid input");
+                 continue;
+             }
+             if (!(result >= 1 && result <= 10))
+             {
+                 Console.WriteLine($"Your input in not range");
+                 continue;
+             }
+
+             int square = result * result;
+             Console.WriteLine($"Square of {result} = {square}");
+
+
+         }*/
+    }
+        /*
 int number1, number2;
 
-        Message();
-        Console.WriteLine("Enter first number");
+    Message();
+    Console.WriteLine("Enter first number");
 number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter Second number");
 number2 = Convert.ToInt32(Console.ReadLine());
 
-        Add(number1, number2);
-        Sub(number1, number2);
-        Mul(number1, number2);
-        Div(number1, number2);
-      
-        //Console.Read();
-    }
+    Add(number1, number2);
+    Sub(number1, number2);
+    Mul(number1, number2);
+    Div(number1, number2);
 
-    public static void Add(int number1, int number2)
+    //Console.Read();   */
+
+
+
+
+    }
+}
+
+   
+     /*   
+        public static void Add(int number1, int number2)
         {
             int result = number1 + number2;
             Console.WriteLine($"{number1} + {number2} = {result}");
@@ -64,7 +125,5 @@ number2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Welcome to Asif,s Calculator");
             Console.WriteLine("Please enter two number");
         }
-
-    }
-
-}
+     */
+    
